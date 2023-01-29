67e3815bd8d97346d763a06bf844dbb1d84ad3df
Yuki Abe <12268087+yukia3e@users.noreply.github.com>
2023-01-29 14:57:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-upgradeable-contract
+ 
---
+++ contracts/VendingMachineV2.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract VendingMachineV2 is Initializable {
+  uint public numSodas;
+  address public owner;
+
+  function initialize(uint _numSodas) public initializer {
+    numSodas = _numSodas;
+    owner = msg.sender;
+  }
+
+  function purchaseSoda() public payable {
+    require(msg.value >= 1000 wei, "You must pay 1000 wei for a soda!");
+    numSodas--;
+  }
+
+  function withdrawProfits() public onlyOwner {
+    require(address(this).balance > 0, "Profits must be greater than 0 in order to withdraw!");
+    (bool sent, ) = owner.call{value: address(this).balance}("");
+    require(sent, "Failed to send ether");
+  }
+
+  function setNewOwner(address _newOwner) public onlyOwner {
+    owner = _newOwner;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only owner can call this function.");
+    _;
+  }
+}
